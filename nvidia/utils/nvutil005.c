a3641bb7754ebcd838c42649f576f68b1758ddb7
Dorian Krause <github@91df63fb.com>
2016-08-16 13:05:07 +0200

    nvutil005: Change whitespaces in output

--- nvidia/utils/nvutil005.c
+++ nvidia/utils/nvutil005.c
@@ -20,18 +20,18 @@ int main(int argc, char **argv)
 	Nvml_Init();
 
 	Nvml_DeviceGetCount(&count);
-	fprintf(stdout, "device count                             : %d\n", (int )count);
+	fprintf(stdout, "device count                            : %d\n", (int )count);
 
 	for (i = 0; i < count; ++i) {
 		Nvml_DeviceGetHandleByIndex(i, &device);
 
 		Nvml_DeviceGetAPIRestriction(device, NVML_RESTRICTED_API_SET_APPLICATION_CLOCKS, &enabled);
-		fprintf(stdout, "device %d application clock restrictions  : %s\n", i, Nvml_Convert_EnableState(enabled));
+		fprintf(stdout, "device %d application clock restrictions : %s\n", i, Nvml_Convert_EnableState(enabled));
 
 		Nvml_DeviceSetAPIRestriction(device, NVML_RESTRICTED_API_SET_APPLICATION_CLOCKS, NVML_FEATURE_DISABLED);
 
 		Nvml_DeviceGetAPIRestriction(device, NVML_RESTRICTED_API_SET_APPLICATION_CLOCKS, &enabled);
-		fprintf(stdout, "device %d application clock restrictions  : %s\n", i, Nvml_Convert_EnableState(enabled));
+		fprintf(stdout, "device %d application clock restrictions : %s\n", i, Nvml_Convert_EnableState(enabled));
 	}
 
 	Nvml_Shutdown();
