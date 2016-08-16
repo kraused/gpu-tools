eac505d90cc4096bc6cdaa7eaebd8859a824cfc3
Dorian Krause <github@91df63fb.com>
2016-08-16 13:05:26 +0200

    Add new nvutil008 and nvutil009 tools
    This commits adds nvutil008 and nvutil009 which are variants
    of nvutil005 that only modify or only read the clock API restriction
    settings, respectively.

---
+++ nvidia/utils/nvutil009.c
@@ -0,0 +1,35 @@
+
+/* nvutil009: Version of nvutil005 that only checks application clock restrictions.
+ */
+
+#include <stdio.h>
+
+#include "common.h"
+
+int main(int argc, char **argv)
+{
+	unsigned int count;
+	unsigned int i;
+	nvmlDevice_t device;
+	nvmlEnableState_t enabled;
+
+#ifndef NDEBUG
+	nvml_return_handler = Nvml_Debug_Return_Handler;
+#endif
+
+	Nvml_Init();
+
+	Nvml_DeviceGetCount(&count);
+	fprintf(stdout, "device count                            : %d\n", (int )count);
+
+	for (i = 0; i < count; ++i) {
+		Nvml_DeviceGetHandleByIndex(i, &device);
+
+		Nvml_DeviceGetAPIRestriction(device, NVML_RESTRICTED_API_SET_APPLICATION_CLOCKS, &enabled);
+		fprintf(stdout, "device %d application clock restrictions : %s\n", i, Nvml_Convert_EnableState(enabled));
+	}
+
+	Nvml_Shutdown();
+	return 0;
+}
+
