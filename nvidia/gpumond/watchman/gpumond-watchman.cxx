6287737b948820c3d8790d48dac59554ebdea623
Dorian Krause <github@91df63fb.com>
2020-06-14 11:56:08 +0200

    gpumond-watchman: Update to latests watchman version
    Write files as unprivileged user and rotate the logs.

--- nvidia/gpumond/watchman/gpumond-watchman.cxx
+++ nvidia/gpumond/watchman/gpumond-watchman.cxx
@@ -1,16 +1,20 @@
 
+#include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <pwd.h>
 
 #include "watchman/plugin.hxx"
 #include "watchman/alloc.hxx"
 #include "watchman/program.hxx"
 #include "watchman/buffer.hxx"
 #include "watchman/file.hxx"
-#include "watchman/named_clingy_file.hxx"
+#include "watchman/named_unpriv_clingy_file.hxx"
+#include "watchman/size_rotator.hxx"
 #include "watchman/watchman.hxx"
 #include "watchman/compiler.hxx"
 #include "watchman/error.hxx"
@@ -23,16 +27,35 @@ static char **_fill_argv()
 	strcpy(_gpumond[0], GPUMOND_BIN_PREFIX "/gpumond.exe");
 
 	_argv[0] = _gpumond[0];
-	_argv[1] = NULL;
+	_argv[1] = nullptr;
 
 	return _argv;
+}
+
+int get_uid_and_gid(char *name, int *uid, int *gid)
+{
+	struct passwd *pwd;
+
+	*uid = 0;
+	*gid = 0;
+
+	pwd = getpwnam(name);
+	if (unlikely(!pwd)) {
+		WATCHMAN_ERROR("getpwnam() failed with errno %d: %s", errno, strerror(errno));
+		return -errno;
+	}
+
+	*uid = pwd->pw_uid;
+	*gid = pwd->pw_gid;
+
+	return 0;
 }
 
 class Gpumond_Program : public Program
 {
 
 public:
-				Gpumond_Program();
+					Gpumond_Program();
 
 };
 
@@ -40,30 +63,32 @@ class Gpumond_Watchman_Plugin : public Watchman_Plugin
 {
 
 public:
-				explicit Gpumond_Watchman_Plugin(void *handle);
+					explicit Gpumond_Watchman_Plugin(void *handle);
 
 public:
-	int			init(Watchman *w, int argc, char **argv);
-	int			fini();
+	int				init(Watchman *w, int argc, char **argv);
+	int				fini();
 
 private:
-	Allocator		*_alloc;
+	Allocator			*_alloc;
 
 private:
-	Gpumond_Program		*_proc;
+	Gpumond_Program			*_proc;
 
 private:
-	Buffer			_buf;
+	Buffer				_buf;
 
 private:
-				/* Mountpoint of the filesystem on which the
-				 * the output file resides. */
-	const char		*_fo_mountpoint;
-	const char		*_fo_path;
+					/* Mountpoint of the filesystem on which the
+					 * the output file resides. */
+	const char			*_fo_mountpoint;
+	const char			*_fo_path;
 
 private:
-	Named_Clingy_File	*_fo;
-	File			*_fe;
+	Named_Unpriv_Clingy_File	*_fo;
+	File				*_fe;
+
+	Size_Rotator			*_rot;
 };
 
 Gpumond_Program::Gpumond_Program()
@@ -72,23 +97,32 @@ Gpumond_Program::Gpumond_Program()
 }
 
 Gpumond_Watchman_Plugin::Gpumond_Watchman_Plugin(void *handle)
-: Watchman_Plugin(handle, 1), _fo(NULL), _fe(NULL)
+: Watchman_Plugin(handle, 1), _fo(nullptr), _fe(nullptr)
 {
 }
 
 int Gpumond_Watchman_Plugin::init(Watchman *w, int argc, char **argv)
 {
-	int   err;
-	off_t offset;
+	int       err;
+	off_t     offset;
+	int       uid, gid;
+	long long threshold;
 
 	_alloc = w->alloc();
 
-	_fo_mountpoint = argv[0];
-	_fo_path       = argv[1];
+	err = get_uid_and_gid(argv[0], &uid, &gid);
+	if (unlikely(err < 0)) {
+		return err;
+	}
+
+	_fo_mountpoint = argv[1];
+	_fo_path       = argv[2];
+
+	threshold = atoll(argv[3]);
 
 	_proc = _alloc->create<Gpumond_Program>();
 
-	_fo = _alloc->create<Named_Clingy_File>();
+	_fo = _alloc->create<Named_Unpriv_Clingy_File>(uid, gid);
 	_fe = _alloc->create<File>(STDERR_FILENO);
 
 	err = _fo->attach(_fo_mountpoint);
@@ -106,7 +140,9 @@ int Gpumond_Watchman_Plugin::init(Watchman *w, int argc, char **argv)
 		WATCHMAN_ERROR("lseek() failed with error %d (%s)", errno, strerror(errno));
 	}
 
-	err = w->add_child(_proc, &_buf, _fo, _fe);
+	_rot = _alloc->create<Size_Rotator>(threshold);
+
+	err = w->add_child(_proc, &_buf, _fo, _fe, _rot);
 	if (unlikely(err)) {
 		WATCHMAN_ERROR("Failed to add children to list: %d", err);
 		return err;
@@ -117,7 +153,9 @@ int Gpumond_Watchman_Plugin::init(Watchman *w, int argc, char **argv)
 
 int Gpumond_Watchman_Plugin::fini()
 {
-	_fo = _alloc->destroy<Named_Clingy_File>(_fo);
+	_rot = _alloc->destroy<Size_Rotator>(_rot);
+
+	_fo = _alloc->destroy<Named_Unpriv_Clingy_File>(_fo);
 	_fe = _alloc->destroy<File>(_fe);
 
 	_proc = _alloc->destroy<Gpumond_Program>(_proc);
